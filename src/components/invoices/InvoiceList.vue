912b025211b384f6a102cb1bbb8be326b4b42784
mihailovasiljevic <mihailonvasiljevic@gmail.com>
2018-04-11 15:59:33 +0200

    Added search bar

--- src/components/invoices/InvoiceList.vue
+++ src/components/invoices/InvoiceList.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
+    <div>
+      <v-layout align-center justify-center>
+          <v-flex xs12 sm12 md12>
+            <v-card class="elevation-1">
+              <v-card-text>
+                <form>
+                  <v-text-field
+                    label="Invoice number"
+                    required
+                  ></v-text-field>
+                </form>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+      </v-layout>
+
+      </div>
+    <div style="overflow-y: scroll; height:400px;">
     <v-list two-line subheader>
             <v-list-tile avatar v-for="invoice in user.invoices" :key="invoice.id" @click="">
               <v-list-tile-content>
@@ -18,7 +36,9 @@
               </v-list-tile-action>
             </v-list-tile>
             </v-list>
+    </div>
   </div>
+
 </template>
 
 
