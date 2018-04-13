10ee5a242a4abe40f527744fc35556a2f38a7c54
mihailovasiljevic <mihailonvasiljevic@gmail.com>
2018-04-13 14:20:44 +0200

    Fixed issues with public folder.

--- public/src/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from './components/Home.vue'
-import Login from './components/users/Login.vue'
-import Invoices from './components/invoices/Invoice.vue'
-
-export const routes = [
-    {path: '/', component: Home},
-    {path: '/login', component: Login},
-    {path: '/:id/invoices', component: Invoices}
-]
